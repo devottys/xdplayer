0b450be3d0ef07f90804a52831ca794e8ff5fb36
Saul Pwanson <code@saul.pw>
2021-02-07 20:34:58 -0800

    move cursor

--- xdplayer.py
+++ xdplayer.py
@@ -2,6 +2,55 @@
 
 import sys
 import curses
+from collections import namedtuple, defaultdict
+
+def getkeystroke(scr):
+    k = scr.get_wch()
+    if isinstance(k, str):
+        if ord(k) >= 32 and ord(k) != 127:  # 127 == DEL or ^?
+            return k
+        k = ord(k)
+    return curses.keyname(k).decode('utf-8')
+
+class ColorMaker:
+    def __init__(self):
+        self.attrs = {}
+        self.color_attrs = {}
+
+        default_bg = curses.COLOR_BLACK
+
+        self.color_attrs['black'] = curses.color_pair(0)
+
+        for c in range(0, 256 or curses.COLORS):
+            try:
+                curses.init_pair(c+1, c, default_bg)
+                self.color_attrs[str(c)] = curses.color_pair(c+1)
+            except curses.error as e:
+                pass # curses.init_pair gives a curses error on Windows
+
+        for c in 'red green yellow blue magenta cyan white'.split():
+            colornum = getattr(curses, 'COLOR_' + c.upper())
+            self.color_attrs[c] = curses.color_pair(colornum+1)
+
+        for a in 'normal blink bold dim reverse standout underline'.split():
+            self.attrs[a] = getattr(curses, 'A_' + a.upper())
+
+    def __getitem__(self, colornamestr):
+        return self._colornames_to_cattr(colornamestr)
+
+    def __getattr__(self, colornamestr):
+        return self._colornames_to_cattr(optname).attr
+
+    def _colornames_to_cattr(self, colornamestr):
+        color, attr = 0, 0
+        for colorname in colornamestr.split(' '):
+            if colorname in self.color_attrs:
+                if not color:
+                    color = self.color_attrs[colorname.lower()]
+            elif colorname in self.attrs:
+                attr = self.attrs[colorname.lower()]
+        return attr | color
+
 
 class AttrDict(dict):
     'Augment a dict with more convenient .attr syntax.  not-present keys return None.'
@@ -27,10 +76,14 @@ class AttrDict(dict):
 
     def __getattr__(self, k):
         try:
-            return self[k][0]
+            v = self[k][0]
+            if k.endswith('attr'):
+                v = colors[v]
+            return v
         except KeyError:
             if k.startswith("__"):
                 raise AttributeError
+
             return None
 
     def __dir__(self):
@@ -38,9 +91,6 @@ class AttrDict(dict):
 
     def cycle(self, k):
         self[k] = self[k][1:] + [self[k][0]]
-
-grid_top = 2
-grid_left = 4
 
 class Crossword:
     def __init__(self, fn):
@@ -54,22 +104,34 @@ class Crossword:
 
         self.solution = gridstr.splitlines()
         self.grid = [
-                [ '#' if x == '#' else '' for x in row ]
+                [ '#' if x == '#' else '.' for x in row ]
                     for row in self.solution 
                 ]
-        self.clues = cluestr.splitlines()
+        self.clues = []
+        for clue in cluestr.splitlines():
+            if clue:
+                clue, answer = clue.split(' ~ ')
+                self.clues.append(clue)
 
-        global grid_bottom, grid_right
+        self.cursor_x = 0
+        self.cursor_y = 0
+
+        global grid_bottom, grid_right, grid_top, grid_left
+        grid_left = 4
+        grid_top = len(self.meta)+1
         grid_bottom = grid_top + len(self.grid)
         grid_right = grid_left + len(self.grid[0])*2
 
         self.options = AttrDict(
-            rowattr = [0, curses.A_UNDERLINE],
+            rowattr = ['', 'underline'],
+            curattr = ['reverse 217'],
+            curacrattr = ['reverse 125'],
+            curdownattr = ['reverse 74'],
 
             topch = '▁_',
-            topattr = [0, curses.A_UNDERLINE],
+            topattr = ['', 'underline'],
             botch = '▇⎴',
-            botattr = curses.A_REVERSE,
+            botattr = ['reverse'],
             midblankch = '█',
             leftblankch = '▌',
             rightblankch = '▐',
@@ -77,7 +139,7 @@ class Crossword:
             leftch = '▊▐│',
             vline = '│┃|┆┇┊┋',
             inside_vline = ' │|┆┃┆┇┊┋',
-            leftattr = [0, curses.A_REVERSE],
+            leftattr = ['', 'reverse'],
             unsolved_char = '· .?□_▁-˙∙•╺‧',
 
             ulch = ' ▗',
@@ -85,62 +147,210 @@ class Crossword:
             blch = ' ▝',
             brch = ' ▘',
             solved = False,
+            hotkeys= False,
         )
 
-    def draw_meta(self, scr):
-        scr.addstr(0, 0, self.meta['Title'])
+        self.pos = defaultdict(list)  # (y,x) -> [(dir, num, answer), ...] associated words with that cell
+        for dir, num, answer, r, c in self.iteranswers_full():
+            for i in range(len(answer)):
+                if dir == 'A':
+                    self.pos[(r,c+i)].append((dir, num, answer))
+                else:
+                    self.pos[(r+i,c)].append((dir, num, answer))
 
-    def draw_grid(self, scr):
+    def cell(self, r, c):
+        if r < 0 or c < 0 or r >= len(self.grid) or c >= len(self.grid[0]):
+            return '#'
+        return self.solution[r][c]
+
+    def iteranswers_full(self):
+        'Generate ("A" or "D", clue_num, answer, r, c) for each word in the grid.'
+
+        NON_ANSWER_CHARS = '_#'
+        clue_num = 1
+        for r, row in enumerate(self.solution):
+            for c, cell in enumerate(row):
+                # compute number shown in box
+                new_clue = False
+                if self.cell(r, c - 1) in NON_ANSWER_CHARS:  # across clue start
+                    ncells = 0
+                    answer = ""
+                    while self.cell(r, c + ncells) not in NON_ANSWER_CHARS:
+                        cellval = self.cell(r, c + ncells)
+                        answer += cellval
+                        ncells += 1
+
+                    if ncells > 1:
+                        new_clue = True
+                        yield "A", clue_num, answer, r, c
+
+                if self.cell(r - 1, c) in NON_ANSWER_CHARS:  # down clue start
+                    ncells = 0
+                    answer = ""
+                    while self.cell(r + ncells, c) not in NON_ANSWER_CHARS:
+                        cellval = self.cell(r + ncells, c)
+                        answer += cellval
+                        ncells += 1
+
+                    if ncells > 1:
+                        new_clue = True
+                        yield "D", clue_num, answer, r, c
+
+                if new_clue:
+                    clue_num += 1
+
+    def draw_meta(self, scr):
+        for y, (k, v) in enumerate(self.meta.items()):
+            scr.addstr(y, 0, '%s: %s' %(k,v))
+
+    def draw_grid(self, scr=None):
         d = self.options
+        cursor_words = self.pos[(self.cursor_y, self.cursor_x)]
+        if cursor_words:
+            cursor_across, cursor_down = sorted(cursor_words)
+        else:
+            cursor_across, cursor_down = None, None
+
         for y, row in enumerate(self.grid):
             for x, ch in enumerate(row):
+                attr = d.rowattr
+                attr2 = d.rowattr
+
                 if ch == '#':
-                    mid = (d.midblankch if x > 0 and row[x-1] == '#' else d.rightblankch) + d.midblankch + (d.midblankch if x < len(row)-1 and row[x+1] == '#' else d.leftblankch)
-                    scr.addstr(grid_top+y, grid_left+x*2-1, mid, d.rowattr)
+                    ch = d.midblankch
+                    ch2 = d.midblankch if x < len(row)-1 and row[x+1] == '#' else d.leftblankch
                 else:
                     ch = self.solution[y][x] if d.solved else d.unsolved_char
-                    scr.addstr(grid_top+y, grid_left+x*2, ch, d.rowattr)
-                    scr.addstr(grid_top+y, grid_left+x*2+1, d.inside_vline, d.rowattr)
+                    ch2 = d.inside_vline
 
-            scr.addstr(grid_top+y, grid_left-1, d.rightblankch if row[0] == '#' else d.vline)
-            scr.addstr(grid_top+y, grid_right-1, d.leftblankch if row[-1] == '#' else d.vline)
+                    words = self.pos[(y,x)]
+                    across_word, down_word = sorted(words)
+                    if cursor_across == across_word and cursor_down == down_word:
+                        attr = d.curattr
+                        attr2 = d.curattr
+                        #ch2 = ' '
+                    elif cursor_across == across_word:
+                        attr = d.curacrattr
+                        attr2 = d.curacrattr
+                        #ch2 = ' '
+                    elif cursor_down == down_word:
+                        attr = d.curdownattr
+                        attr2 = d.curdownattr
+                        #ch2 = ' '
 
-        scr.addstr(grid_top-1, grid_left, d.topch*(len(self.grid[0])*2-1), d.topattr)
-        scr.addstr(grid_bottom,grid_left, d.botch*(len(self.grid[0])*2-1), d.rowattr | d.botattr)
+                if scr:
+                    scr.addstr(grid_top+y, grid_left+x*2, ch, attr)
+                    scr.addstr(grid_top+y, grid_left+x*2+1, ch2, attr2)
 
-        scr.addstr(grid_top-1, grid_left-1, d.ulch)
-        scr.addstr(grid_bottom,grid_left-1, d.blch)
-        scr.addstr(grid_top-1, grid_right-1, d.urch)
-        scr.addstr(grid_bottom,grid_right-1, d.brch)
+                if x == 0:
+                    if ch == '#' or cursor_down == down_word:
+                        ch = d.leftblankch
+                    else:
+                        ch = d.vline
+                    scr.addstr(grid_top+y, grid_left-1, ch, attr)
 
-        scr.move(0,0)
+                if x == len(row)-1:
+                    if ch == '#' or cursor_down == down_word:
+                        ch = d.rightblankch
+                    else:
+                        ch = d.vline
+                    scr.addstr(grid_top+y, grid_right-1, ch, attr)
+
+
+        if scr:
+            scr.addstr(grid_top-1, grid_left, d.topch*(len(self.grid[0])*2-1), d.topattr)
+            scr.addstr(grid_bottom,grid_left, d.botch*(len(self.grid[0])*2-1), d.rowattr | d.botattr)
+
+            scr.addstr(grid_top-1, grid_left-1, d.ulch)
+            scr.addstr(grid_bottom,grid_left-1, d.blch)
+            scr.addstr(grid_top-1, grid_right-1, d.urch)
+            scr.addstr(grid_bottom,grid_right-1, d.brch)
+
+            scr.move(0,0)
 
     def draw_clues(self, scr):
-        pass
+        for y, clue in enumerate(self.clues):
+            scr.addstr(grid_bottom+y+1, 1, clue)
+            if y > 5: break
 
     def draw(self, scr):
-        scr.erase()
         self.draw_meta(scr)
         self.draw_grid(scr)
         self.draw_clues(scr)
 
-def main(scr):
-    xd = Crossword(sys.argv[1])
-    while True:
-        opt = xd.options
-        xd.draw(scr)
-        hotkeys = {}
-        for y, (k, v) in enumerate(opt.items()):
+
+    def draw_hotkeys(self, scr):
+        self.hotkeys = {}
+        for y, (k, v) in enumerate(self.options.items()):
             key = "0123456789abcdefghijklmnopqrstuvwxyz"[y]
-            hotkeys[key] = k
+            self.hotkeys[key] = k
 
             scr.addstr(grid_top+y, 40, key)
             scr.addstr(grid_top+y, 45, k)
             scr.addstr(grid_top+y, 60, ' '.join(map(str, v)))
 
-        k = scr.getkey()
 
-        if k == 'q': return
-        if k in hotkeys: opt.cycle(hotkeys[k])
+class CrosswordPlayer:
+    def __init__(self):
+        self.statuses = []
+        self.xd = None
+        self.n = 0
+
+    def status(self, s):
+        self.statuses.append(s)
+
+    def play_one(self, scr, xd):
+        h, w = scr.getmaxyx()
+        opt = xd.options
+        xd.draw(scr)
+        if self.statuses:
+            scr.addstr(h-1, 10, self.statuses.pop())
+        if opt.hotkeys:
+            xd.draw_hotkeys(scr)
+        k = getkeystroke(scr)
+        if k == 'q': return True
+
+        scr.erase()
+        scr.addstr(h-1, 0, k)
+        scr.addstr(h-1, 40, str(self.n))
+        self.n += 1
+
+        if k == 'KEY_MOUSE':
+            devid, x, y, z, bstate = curses.getmouse()
+            if grid_top <= y < grid_bottom and grid_left <= x < grid_right:
+                x = (x-grid_left)//2
+                y = y-grid_top
+                if xd.grid[y][x] != '#':
+                    xd.cursor_x = x
+                    xd.cursor_y = y
+            else:
+                self.status('not on grid')
+
+        elif k == 'KEY_DOWN': xd.cursor_y += 1
+        elif k == 'KEY_UP': xd.cursor_y -= 1
+        elif k == 'KEY_LEFT': xd.cursor_x -= 1
+        elif k == 'KEY_RIGHT': xd.cursor_x += 1
+        elif k == '^X':
+            opt.hotkeys = not opt.hotkeys
+            return 
+
+        if opt.hotkeys and k in xd.hotkeys:
+            opt.cycle(xd.hotkeys[k])
+
+
+def main(scr):
+    global colors
+
+    curses.use_default_colors()
+    curses.raw()
+    curses.meta(1)
+    curses.mousemask(-1)
+
+    colors = ColorMaker()
+
+    plyr = CrosswordPlayer()
+    xd = Crossword(sys.argv[1])
+    while not plyr.play_one(scr, xd):
+        pass
 
 curses.wrapper(main)
