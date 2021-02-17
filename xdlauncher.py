13b33cb795347a20d4f78c677afbd0790c13a40b
Saul Pwanson <code@saul.pw>
2021-02-16 23:36:24 -0800

    [xdlauncher] add modtime, rearrange rows

--- xdlauncher.py
+++ xdlauncher.py
@@ -2,31 +2,33 @@
 
 import os
 import subprocess
+import stat
 
 import visidata
-from visidata import SqliteQuerySheet, Path, vd, Column, SuspendCurses
+from visidata import SqliteQuerySheet, Path, vd, Column, SuspendCurses, date
 
 
 class xdLauncher(SqliteQuerySheet):
     query='''SELECT xdmeta.xdid,
+                    solvings.teamid,
+                    solvings.correct,
+                    solvings.nonblocks,
                     size,
                     title,
                     author,
                     editor,
                     copyright,
                     date_published,
-                    solvings.teamid,
-                    solvings.correct,
-                    solvings.nonblocks,
                     path
                 FROM xdmeta
                 LEFT OUTER JOIN solvings ON xdmeta.xdid = solvings.xdid'''
     columns = [
-        Column('mtime'),
+        Column('modtime', width=0, type=date, getter=lambda c,r: Path(r[-1]).stat()[stat.ST_MTIME]),
     ]
+    _ordering = [('modtime', True)]  # sort by reverse modtime initially
     def openRow(self, row):
         with SuspendCurses():
             return subprocess.call(['./xdplayer.py', row[-1]])
 
 
-visidata.run(xdLauncher(source=Path(os.getenv('XDDB', 'xd.db'))))
+visidata.run(xdLauncher('xd_launcher', source=Path(os.getenv('XDDB', 'xd.db'))))
