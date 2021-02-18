87b5d8648302631a8b6690f7c881fe1fc781441a
anjakefala <anja.kefala@gmail.com>
2021-02-18 05:08:51 +0000

    minor bugfixes to check_recent

--- check_recent.sh
+++ check_recent.sh
@@ -2,13 +2,13 @@
 
 BINDIR=/opt/xdplayer
 
-cd /opt/teams/
+$TEAMDIR=/opt/teams
 
 for TEAMID in * ; do
-  for guesspath in $(find $TEAMDIR -name \*.jsonl -mmin -60) ; do
+  for guesspath in $(find $TEAMDIR/$TEAMID -name \*.jsonl -mmin -60) ; do
     guessfn=$(basename -- "$guesspath")
     xdid=${guessfn%%.xd-guesses.jsonl}
-    xdpath="$(./xdid2path.py $xdid)"
-    TEAMDIR="/opt/teams/$TEAMID" $BINDIR/xdiff.py $xdpath
+    xdpath="$($BINDIR/xdid2path.py $xdid)"
+    TEAMDIR="$TEAMDIR/$TEAMID" $BINDIR/xdiff.py $xdpath
   done
 done
