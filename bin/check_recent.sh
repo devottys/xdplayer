b8b2e55cdcc8cc5fe915975b62b4504b1ceccf47
anjakefala <anja.kefala@gmail.com>
2021-02-18 17:11:52 +0000

    Fix TEAMID and TEAMDIR references

--- bin/check_recent.sh
+++ bin/check_recent.sh
@@ -4,11 +4,11 @@ BINDIR=/opt/xdplayer
 
 TEAMDIR=/opt/teams
 
-for TEAMID in * ; do
-  for guesspath in $(find $TEAMDIR/$TEAMID -name \*.jsonl -mmin -60) ; do
+for TEAMID in $TEAMDIR/* ; do
+  for guesspath in $(find $TEAMID -name \*.jsonl -mmin -60) ; do
     guessfn=$(basename -- "$guesspath")
     xdid=${guessfn%%.xd-guesses.jsonl}
     xdpath="$($BINDIR/xdid2path.py $xdid)"
-    TEAMDIR="$TEAMDIR/$TEAMID" $BINDIR/xdiff.py $xdpath
+    TEAMDIR="$TEAMID" $BINDIR/xdiff.py $xdpath
   done
 done
