ed22fd15e3ef8622bb95bd3d5840fdc997a8e1f2
Saul Pwanson <code@saul.pw>
2021-02-17 21:21:42 -0800

    [adduser] setup_user <username> <teamid>

--- setup_user.sh
+++ setup_user.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-TEAMID='teamxd'
+USER="$1"
+TEAMID="$2"
 TEAMDIR="/opt/teams/$TEAMID"
 
-XDDIR='/opt/gxd/'
-XDDB='/opt/teams/xd.db'
+XDDIR="/opt/gxd/"
+XDDB="/opt/teams/xd.db"
 
 mkdir -p $TEAMDIR
-ln -s $TEAMDIR ~/$TEAMID
+chown $USER $TEAMDIR
+chmod ugo+rwx $TEAMDIR
 
-echo -e "TEAMID=$TEAMID\nTEAMDIR=$TEAMDIR\nXDDIR=$XDDIR\nXDDB=$XDDB" >> ~/.bashrc
+echo -e "TEAMID=$TEAMID\nTEAMDIR=$TEAMDIR\nXDDIR=$XDDIR\nXDDB=$XDDB" >> $home/$USER/.bashrc
