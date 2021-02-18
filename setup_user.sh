2408a74d6084ec022f4ac67e66a4c7e73eed07f3
anjakefala <anja.kefala@gmail.com>
2021-02-17 21:11:08 -0800

    Users do not need to clone xdplayer anymore

--- setup_user.sh
+++ setup_user.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 TEAMID='teamxd'
-TEAMDIR='/opt/teams/teamxd'
+TEAMDIR="/opt/teams/$TEAMID"
+
 XDDIR='/opt/gxd/'
 XDDB='/opt/teams/xd.db'
 
+mkdir -p $TEAMDIR
 ln -s $TEAMDIR ~/$TEAMID
-# user will need to have access to github for this to work
-git clone git@github.com:devotees/xdplayer.git
 
 echo -e "TEAMID=$TEAMID\nTEAMDIR=$TEAMDIR\nXDDIR=$XDDIR\nXDDB=$XDDB" >> ~/.bashrc
