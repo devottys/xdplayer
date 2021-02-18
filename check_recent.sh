863448c4e97cdd8cdb3c62dff7ba3c9a3bbf037b
anjakefala <anja.kefala@gmail.com>
2021-02-17 16:59:09 -0800

    Find changes in the last hour

--- check_recent.sh
+++ check_recent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for guesspath in $(find $TEAMDIR -name \*.jsonl -mmin +60) ; do
+for guesspath in $(find $TEAMDIR -name \*.jsonl -mmin -60) ; do
     guessfn=$(basename -- "$guesspath")
     xdid=${guessfn%%.xd-guesses.jsonl}
     xdpath="$(./xdid2path.py $xdid)"
