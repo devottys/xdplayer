82d6700defca348e7efde8b093eee3f76c1132c2
Saul Pwanson <code@saul.pw>
2021-02-20 20:58:42 -0800

    [xdlauncher] stat guesses file for modtime, solvetime, and is_submitted

--- bin/xdlauncher.py
+++ bin/xdlauncher.py
@@ -5,8 +5,33 @@ import subprocess
 import stat
 
 import visidata
-from visidata import SqliteQuerySheet, Path, vd, Column, SuspendCurses, date
+from visidata import SqliteQuerySheet, Path, vd, Column, SuspendCurses, date, VisiData
 
+
+def stat_guesses(fn):
+    'Return Path($TEAMDIR/{fn.stem}.xd-guesses.jsonl'
+    xdid = Path(fn).stem
+    p = Path(os.getenv('TEAMDIR', '.'))/(xdid+'.xd-guesses.jsonl')
+    return p.stat()
+
+def is_submitted(fn):
+    'Return True if exists and is readonly.'
+    g = stat_guesses(fn)
+    if not g:
+        return False
+    return not (stat_guesses(fn).st_mode & stat.S_IWUSR)
+
+def modtime(fn):
+    g = stat_guesses(fn)
+    if not g:
+        return None
+    return g.st_mtime
+
+def solvetime(fn):
+    g = stat_guesses(fn)
+    if not g:
+        return None
+    return g.st_ctime - g.st_mtime
 
 class xdLauncher(SqliteQuerySheet):
     query='''SELECT xdmeta.xdid,
@@ -23,7 +48,9 @@ class xdLauncher(SqliteQuerySheet):
                 FROM xdmeta
                 LEFT OUTER JOIN solvings ON xdmeta.xdid = solvings.xdid'''
     columns = [
-        Column('modtime', width=0, type=date, getter=lambda c,r: Path(r[-1]).stat()[stat.ST_MTIME]),
+        Column('modtime', width=0, type=date, getter=lambda c,r: modtime(r[-1])),
+        Column('submitted', width=0, getter=lambda c,r: is_submitted(r[-1])),
+        Column('solve_secs', type=int, getter=lambda c,r: solvetime(r[-1])),
     ]
     _ordering = [('modtime', True)]  # sort by reverse modtime initially
     def openRow(self, row):
